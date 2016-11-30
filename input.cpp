bb4eb0e8024eb04be3c1900a717ac5c7e9e9f012
pablitocalvo <sbtpla68@yahoo.it>
2016-11-30 22:20:47 +0100

    aggiunto learning c++ by listening

---
+++ input.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int n = 0;
+		
+	if ( n==0 ) 
+		cout<<"n è uguale a 0"<<endl;
+	else 
+		cout<<"n non è zero"<<endl;
+		
+	
+	n=1;
+	
+	if ( n==0 ) 
+		cout<<"n è uguale a 0"<<endl;
+	else 
+		cout<<"n non è zero"<<endl;
+		
+	
+	
+}
+
---
+++ myScript.js
@@ -0,0 +1,34 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("p").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "soluzione.txt");
+  xhttp.send();
+};
+			function funz() {
+ 			  tac=document.getElementById("ta").value
+ 			  pc=document.getElementById("p").innerHTML;
+ 
+ 			  alert(tac+"\r"+pc)
+ 			  
+ 			  var match = /\r|\n/.exec(tac);
+				if (match) {
+    					alert("tac ne ha !")
+					} 
+				match = /\r|\n/.exec(pc);
+				if (match) {
+    					alert("pc ne ha !")
+					}
+ 			  loadDoc()
+
+				match = /\r|\n/.exec(pc);
+				if (match) {
+    					alert("pc ne ha !")
+					}
+ 			  loadDoc()
+ 			  
+ 			  
+ 			}
